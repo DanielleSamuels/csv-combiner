0e1c762d40e7b6ed61e24baebb2e4d4681533839
DanielleSamuels <99514757+DanielleSamuels@users.noreply.github.com>
2023-02-07 09:03:51 -0600

    All functionality implemented except output file via command line argument

--- csv-combiner.py
+++ csv-combiner.py
@@ -2,18 +2,25 @@ import sys
 import csv
 import os
 
-# sys.argv
+# gets number of comand line arguments
 numFiles = len(sys.argv)
-# for i in range(1, numFiles):
-  #  print(sys.argv[i], end = " ")
 
+# FOR TESTING - REMOVE LATER
 outputFile = "out.csv"
 
-with open(sys.argv[1], 'r') as file:
-  csvreader = csv.reader(file)
-  next(csvreader)    # skip first row
-  for row in csvreader:
-    if row:  # only continue if row is not empty             
-        newRow = row
-        newRow.append(os.path.basename(sys.argv[1]).split('/')[-1]) # os extracts file name from the file path
-        print(newRow)
+# opens writer and sets header
+with open(outputFile, 'w', newline='') as outfile:
+  csvwriter = csv.writer(outfile, delimiter=',')
+  csvwriter.writerow(['email_hash', 'category', 'filename'])
+  # loop for the number of files
+  for i in range(1, numFiles):
+    # open reader
+    with open(sys.argv[i], 'r') as infile:
+      csvreader = csv.reader(infile)
+      next(csvreader)    # skip first row
+      for row in csvreader:
+        if row:  # only continue if row is not empty             
+            # add 3rd column
+            newRow = row
+            newRow.append(os.path.basename(sys.argv[1]).split('/')[-1]) # os extracts file name from the file path
+            csvwriter.writerow(newRow)
