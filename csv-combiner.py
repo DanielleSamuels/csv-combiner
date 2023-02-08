340cedf7ba15061f5d71a97cbb59ddd91b2a76b2
DanielleSamuels <99514757+DanielleSamuels@users.noreply.github.com>
2023-02-08 11:55:37 -0600

    Almost done - just need testers + error checking

--- csv-combiner.py
+++ csv-combiner.py
@@ -5,15 +5,12 @@ import os
 # gets number of comand line arguments
 numFiles = len(sys.argv)
 
-# FOR TESTING - REMOVE LATER
-outputFile = "out.csv"
-
 # opens writer and sets header
-with open(outputFile, 'w', newline='') as outfile:
-  csvwriter = csv.writer(outfile, delimiter=',')
-  csvwriter.writerow(['email_hash', 'category', 'filename'])
-  # loop for the number of files
-  for i in range(1, numFiles):
+csvwriter = csv.writer(sys.stdout, doublequote=False, escapechar='\\', quoting=csv.QUOTE_ALL)
+csvwriter.writerow(['email_hash', 'category', 'filename'])
+
+# loop for the number of files
+for i in range(1, numFiles):
     # open reader
     with open(sys.argv[i], 'r') as infile:
       csvreader = csv.reader(infile)
@@ -21,6 +18,8 @@ with open(outputFile, 'w', newline='') as outfile:
       for row in csvreader:
         if row:  # only continue if row is not empty             
             # add 3rd column
-            newRow = row
-            newRow.append(os.path.basename(sys.argv[1]).split('/')[-1]) # os extracts file name from the file path
-            csvwriter.writerow(newRow)
+            row.append(os.path.basename(sys.argv[i])) # os extracts file name from the file path
+            csvwriter.writerow(row)
+            
+# close files
+infile.close()
